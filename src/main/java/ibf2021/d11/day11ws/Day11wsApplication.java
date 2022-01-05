e13c3e27e094f478228e025bd8db9889d3f0dc1d
Mac <mac@Macs-MacBook-Pro.local>
2022-01-05 15:10:10 +0800

    new

---
+++ src/main/java/ibf2021/d11/day11ws/Day11wsApplication.java
@@ -0,0 +1,13 @@
+package ibf2021.d11.day11ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11wsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf2021/d11/day11ws/Day11wsApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.d11.day11ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11wsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
